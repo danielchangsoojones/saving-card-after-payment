33e1ed2eec85cdecf2497b8f14a8c3b8fa0c1631
Daniel Jones <messyjones@gmail.com>
2021-02-12 19:34:38 -0500

    added the static

--- without-webhooks/server/node/server.js
+++ without-webhooks/server/node/server.js
@@ -3,9 +3,9 @@ const app = express();
 const { resolve } = require("path");
 // Copy the .env.example in the root into a .env file in this folder
 const env = require("dotenv").config({ path: "./.env" });
-const stripe = require("stripe")(env.parsed.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-app.use(express.static(env.parsed.STATIC_DIR));
+app.use(express.static(process.env.STATIC_DIR));
 app.use(
   express.json({
     // We need the raw body to verify webhook signatures.
@@ -20,12 +20,12 @@ app.use(
 
 app.get("/", (req, res) => {
   // Display checkout page
-  const path = resolve(env.parsed.STATIC_DIR + "/index.html");
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
   res.sendFile(path);
 });
 
 app.get("/stripe-key", (req, res) => {
-  res.send({ publicKey: env.parsed.STRIPE_PUBLISHABLE_KEY });
+  res.send({ publicKey: process.env.STRIPE_PUBLISHABLE_KEY });
 });
 
 const calculateOrderAmount = items => {
