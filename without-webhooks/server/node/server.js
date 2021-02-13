c7c07449b20c5780d0a591568dfb65d011a264da
Daniel Jones <messyjones@gmail.com>
2021-02-12 20:19:41 -0500

    it now can save the card by default

--- without-webhooks/server/node/server.js
+++ without-webhooks/server/node/server.js
@@ -60,15 +60,14 @@ app.post("/pay", async (req, res) => {
         confirm: true
       };
 
-      if (isSavingCard) {
-        // Create a Customer to store the PaymentMethod
-        const customer = await stripe.customers.create();
-        paymentIntentData.customer = customer.id;
-        
-        // setup_future_usage saves the card and tells Stripe how you plan to use it later
-        // set to "off_session" if you plan on charging the saved card when your user is not present
-        paymentIntentData.setup_future_usage = 'off_session';
-      }
+      //this saves the card for future use
+      // Create a Customer to store the PaymentMethod
+      const customer = await stripe.customers.create();
+      paymentIntentData.customer = customer.id;
+      
+      // setup_future_usage saves the card and tells Stripe how you plan to use it later
+      // set to "off_session" if you plan on charging the saved card when your user is not present
+      paymentIntentData.setup_future_usage = 'off_session';
 
       intent = await stripe.paymentIntents.create(paymentIntentData);
     } else {
