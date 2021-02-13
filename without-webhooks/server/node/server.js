ab38cff823fd004f5d88440e0fc70104bd75dc19
Daniel Jones <messyjones@gmail.com>
2021-02-12 20:09:20 -0500

    moved the files

--- without-webhooks/server/node/server.js
+++ without-webhooks/server/node/server.js
@@ -22,7 +22,7 @@ app.use(
 
 app.get("/", (req, res) => {
   // Display checkout page
-  const thePath = path.resolve("../../client/index.html");
+  const thePath = path.resolve("/client/index.html");
   res.sendFile(thePath);
 });
 
